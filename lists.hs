78245b592567be37249d196a64207aa0e25fe2fc
mafinar <mafinar@gmail.com>
2018-08-08 23:53:37 -0400

    Refactor camelCase and WHERE

--- lists.hs
+++ lists.hs
@@ -1,17 +1,17 @@
 -- P01 (*) Find the last element of a list.
-my_last :: [a] -> a
-my_last [] = error "Empty list"
-my_last [x] = x
-my_last (_:xs) = my_last xs
+lastElem :: [a] -> a
+lastElem [] = error "Empty list"
+lastElem [x] = x
+lastElem (_:xs) = lastElem xs
 
 
 -- P02 (*) Find the last but one element of a list.
-my_last_but_one :: [a] -> a
-my_last_but_one [] = error "Empty list"
-my_last_but_one [_] = error "Only one element"
-my_last_but_one [x, _] = x
-my_last_but_one [_, x, _] = x
-my_last_but_one (_:xs) = my_last_but_one xs
+penultimate :: [a] -> a
+penultimate [] = error "Empty list"
+penultimate [_] = error "Only one element"
+penultimate [x, _] = x
+penultimate [_, x, _] = x
+penultimate (_:xs) = penultimate xs
 
 
 -- P03 (*) Find the K'th element of a list.
@@ -20,22 +20,18 @@ kth [] _ = error "Index out of bounds"
 kth _ 0 = error "Zero index"
 kth (x:xs) 1 = x
 kth (_:xs) n
-  | n < 1 = error "Index out of bounds"
-  | otherwise = kth xs (pred n)
+    | n < 1 = error "Index out of bounds"
+    | otherwise = kth xs (pred n)
 
 -- P04 (*) Find the number of elements of a list.
 size :: [a] -> Int
-size x = size' x 0
-
-size' :: [a] -> Int -> Int
-size' [] n = n
-size' (x:xs) n = size' xs (succ n)
+size x = size' x 0 where
+    size' [] n = n
+    size' (x:xs) n = size' xs (succ n)
 
 -- P05 (*) Reverse a list.
-reverse' :: [a] -> [a]
-reverse' x = reverse'' x []
-
-reverse'' :: [a] -> [a] -> [a]
-reverse'' [] xs = xs
-reverse'' (x:xs) xss = reverse'' xs (x:xss)
+reversed :: [a] -> [a]
+reversed x = reversed' x [] where
+    reversed' [] xs = xs
+    reversed' (x:xs) xss = reversed' xs (x:xss)
 
