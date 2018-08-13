7d26be394e3f1694e08e096096bfa1886e409ca8
mafinar <mafinar@gmail.com>
2018-08-13 00:32:50 -0400

    Problem 8

--- lists.hs
+++ lists.hs
@@ -46,3 +46,10 @@ data MaybeNested a = Elem a | List [MaybeNested a]
 flatten :: MaybeNested a -> [a]
 flatten (Elem x) = [x]
 flatten (List x) = concatMap flatten x
+
+-- P08 (**) Eliminate consecutive duplicates of list elements.
+compress :: Eq a => [a] -> [a]
+compress (x:xs@(y:_))
+    | x == y = compress xs
+    | otherwise = x : compress xs
+compress xs = xs
